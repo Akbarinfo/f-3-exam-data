0c084d39c3ce0558b1aaa6c9bb113c164cdf8669
roziqov1011 <roziqov1011@gmail.com>
2022-04-26 15:38:17 +0500

    initial commit

---
+++ data.js
@@ -0,0 +1,102 @@
+const pokemons = [
+    {
+      id:1,
+      typeId:1,
+      img: "https://www.serebii.net/pokemongo/pokemon/001.png"
+    },
+    {
+      id:2,
+      typeId:1,
+      img: "https://www.serebii.net/pokemongo/pokemon/001.png"
+    },
+    {
+      id:3,
+      typeId:2,
+      img: "https://www.serebii.net/pokemongo/pokemon/002.png"
+    },
+    {
+      id:4,
+      typeId:2,
+      img: "https://www.serebii.net/pokemongo/pokemon/002.png"
+    },
+    {
+      id:5,
+      typeId:3,
+      img: "https://www.serebii.net/pokemongo/pokemon/004.png"
+    },
+    {
+      id:6,
+      typeId:3,
+      img: "https://www.serebii.net/pokemongo/pokemon/004.png"
+    },
+    {
+      id:7,
+      typeId:4,
+      img: "https://www.serebii.net/pokemongo/pokemon/005.png"
+    },
+    {
+      id:8,
+      typeId:4,
+      img: "https://www.serebii.net/pokemongo/pokemon/005.png"
+    },
+    {
+      id:9,
+      typeId:5,
+      img: "https://www.serebii.net/pokemongo/pokemon/006.png"
+    },
+    {
+      id:10,
+      typeId:5,
+      img: "https://www.serebii.net/pokemongo/pokemon/006.png"
+    },
+    {
+      id:11,
+      typeId:6,
+      img: "https://www.serebii.net/pokemongo/pokemon/007.png"
+    },
+    {
+      id:12,
+      typeId:6,
+      img: "https://www.serebii.net/pokemongo/pokemon/007.png"
+    },
+    {
+      id:13,
+      typeId:7,
+      img: "https://www.serebii.net/pokemongo/pokemon/008.png"
+    },
+    {
+      id:14,
+      typeId:7,
+      img: "https://www.serebii.net/pokemongo/pokemon/008.png"
+    },
+    {
+      id:15,
+      typeId:8,
+      img: "https://www.serebii.net/pokemongo/pokemon/009.png"
+    },
+    {
+      id:16,
+      typeId:8,
+      img: "https://www.serebii.net/pokemongo/pokemon/009.png"
+    },
+    {
+      id:17,
+      typeId:9,
+      img: "https://www.serebii.net/pokemongo/pokemon/010.png"
+    },
+    {
+      id:18,
+      typeId:9,
+      img: "https://www.serebii.net/pokemongo/pokemon/010.png"
+    },
+    {
+      id:19,
+      typeId:10,
+      img: "https://www.serebii.net/pokemongo/pokemon/011.png"
+    },
+    {
+      id:20,
+      typeId:10,
+      img: "https://www.serebii.net/pokemongo/pokemon/011.png"
+    },
+  ]
